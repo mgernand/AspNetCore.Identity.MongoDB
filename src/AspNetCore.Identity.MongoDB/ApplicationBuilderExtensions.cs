425ea914245fe74898c9ab6a6ec7646a44c7a644
Matthias Gernand <mgernand@users.noreply.github.com>
2023-03-28 16:34:12 +0200

    Removed the filter for registering the convention pack. (#12)

--- src/AspNetCore.Identity.MongoDB/ApplicationBuilderExtensions.cs
+++ src/AspNetCore.Identity.MongoDB/ApplicationBuilderExtensions.cs
@@ -1,6 +1,5 @@
 ﻿namespace MadEyeMatt.AspNetCore.Identity.MongoDB
 {
-	using System;
 	using System.Collections.Generic;
 	using System.Threading.Tasks;
 	using global::MongoDB.Bson.Serialization.Conventions;
@@ -39,39 +38,14 @@
 			{
 				pack.Add(new DataProtectionConvention(applicationBuilder.ApplicationServices));
 			}
-
-			ConventionRegistry.Register("IdentityConventionPack", pack, type =>
-				IsGenericBaseType(type, typeof(IdentityUser<>)) ||
-				IsGenericBaseType(type, typeof(MongoIdentityUser<>)) ||
-				IsGenericBaseType(type, typeof(IdentityRole<>)) ||
-				IsGenericBaseType(type, typeof(MongoIdentityRole<>)) ||
-				type == typeof(MongoClaim) ||
-				type == typeof(MongoUserLogin) ||
-				type == typeof(MongoUserToken) ||
-				type == typeof(MongoClaim));
+			
+			ConventionRegistry.Register("IdentityConventionPack", pack, _ => true);
 
 			IEnumerable<IEnsureSchema> ensureSchemata = applicationBuilder.ApplicationServices.GetServices<IEnsureSchema>();
 			foreach(IEnsureSchema ensureSchema in ensureSchemata)
 			{
 				await ensureSchema.ExecuteAsync();
 			}
-		}
-
-		private static bool IsGenericBaseType(Type currentType, Type genericBaseType)
-		{
-			Type type = currentType;
-			while(type != null)
-			{
-				Type genericType = type.IsGenericType ? type.GetGenericTypeDefinition() : null;
-				if(genericType != null && genericType == genericBaseType)
-				{
-					return true;
-				}
-
-				type = type.BaseType;
-			}
-
-			return false;
 		}
 	}
 }
