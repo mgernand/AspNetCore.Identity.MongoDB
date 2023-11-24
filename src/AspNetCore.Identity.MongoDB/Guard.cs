a793005e86082c2c76e0330fc0096df83281108b
Matthias Gernand <mgernand@users.noreply.github.com>
2023-11-24 13:53:03 +0100

    Added support for .NET 6 and .NET 7 (#28)

---
+++ src/AspNetCore.Identity.MongoDB/Guard.cs
@@ -0,0 +1,29 @@
+﻿namespace MadEyeMatt.AspNetCore.Identity.MongoDB
+{
+	using System.Runtime.CompilerServices;
+	using System;
+	using System.Diagnostics.CodeAnalysis;
+
+	internal static class Guard
+	{
+		internal static void ThrowIfNull([NotNull] object argument, [CallerArgumentExpression("argument")] string parameterName = null)
+		{
+			if (argument != null)
+			{
+				return;
+			}
+
+			throw new ArgumentNullException(parameterName);
+		}
+
+		public static void ThrowIfNullOrWhiteSpace([NotNull] string argument, [CallerArgumentExpression("argument")] string parameterName = null)
+		{
+			if (!string.IsNullOrWhiteSpace(argument))
+			{
+				return;
+			}
+
+			throw new ArgumentNullException(argument, parameterName);
+		}
+	}
+}
